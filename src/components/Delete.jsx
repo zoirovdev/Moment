45627968ac73c4d3f01985342f2327a753f4d285
zoirovdev <abboszoirov17@gmail.com>
2025-08-20 11:31:08 +0500

    Update UI

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -2,9 +2,26 @@
 
 
 
-const Delete = () => {
+const Delete = ({setDeleteModal}) => {
   return (
-    <div>Delete</div>
+    <div className="fixed inset-0 flex justify-center items-center z-50">
+          <div className="bg-white p-6 space-y-6 rounded-lg shadow-xl text-black">
+            <p className="mb-4">Are you sure you want to delete this note?</p>
+            <div className="flex gap-1">
+              <button onClick={() => setDeleteModal(false)}
+                className="px-4 py-1 w-[50%] bg-gray-300 text-black rounded-[20px] hover:bg-gray-400">
+                Cancel
+              </button>
+              <button onClick={() => {
+                  // Add delete logic here
+                  setDeleteModal(false);
+                }}
+                className="px-4 py-1 w-[50%] bg-red-500 text-white rounded-[20px] hover:bg-red-600">
+                Delete
+              </button>
+            </div>
+          </div>
+    </div>
   )
 }
 
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,7 +1,7 @@
 import { ArrowLeft, SquarePen, Trash2 } from "lucide-react"
 import { Link } from "react-router"
 import { useState } from "react"
-
+import Delete from "../components/Delete.jsx"
 
 
 
@@ -38,24 +38,7 @@ const View = () => {
       </div>
 
       {deleteModal && 
-        <div className="fixed inset-0 flex justify-center items-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-xl text-black">
-            <p className="mb-4">Are you sure you want to delete this note?</p>
-            <div className="flex gap-2">
-              <button onClick={() => setDeleteModal(false)}
-                className="px-4 py-2 bg-gray-300 text-black rounded hover:bg-gray-400">
-                Cancel
-              </button>
-              <button onClick={() => {
-                  // Add delete logic here
-                  setDeleteModal(false);
-                }}
-                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+        <Delete setDeleteModal={setDeleteModal}/>
       }
     </div>
   )
